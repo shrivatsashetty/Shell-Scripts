7d222daad410c0489f53a2972034ae3949fbc889
shrivatsashetty <shrivatsa307@gmail.com>
2023-04-30 18:33:11 +0530

    made some corrections

--- first.sh
+++ first.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-VAR1 = 123456
-TEST = RVCEMCA
+VAR1=123456
+TEST="RVCEMCA"
 #DOUBLE QUOTES
 echo "this is Double quote $VAR1 $TEST "
 #SINGLE QOOUTES
-echo 'this is a single quote $VAR1 $TEST'
+echo "this is a single quote $VAR1 $TEST"
 #REVERSE QUOTE / back tick
-echo "this host(computer) name is :`hostname`"
+echo "this host(computer) name is :$(hostname)"
