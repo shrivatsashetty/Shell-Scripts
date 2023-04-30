fe34a955c891b90dfd73b20836584bb19c72f4e4
shrivatsashetty <shrivatsa307@gmail.com>
2023-04-30 20:22:53 +0530

    arithmetic operation

--- arithmetics.sh
+++ arithmetics.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
 
 # illustration of arithmetic operators
-echo "ARITHMETIC OPERATIONS"
-echo ""
-echo "Enter a: "
+# echo "ARITHMETIC OPERATIONS"
+# echo ""
+# echo "Enter a: "
+# read -r a
+# echo "enter b: "
+# read -r b
+# echo "$a + $b = $((a+b))"
+# echo "$a - $b = $((a-b))"
+# echo "$a * $b = $((a*b))"
+# echo "$a mod $b = $((a%b))"
+# echo "$a/$b = $((a/b))"
+# echo "Thats it, thank you"
+echo "Arithemetic operations"
+echo "Enter a value"
 read -r a
-echo "enter b: "
+echo "Enter b value"
 read -r b
-echo "$a + $b = $((a+b))"
-echo "$a - $b = $((a-b))"
-echo "$a * $b = $((a*b))"
-echo "$a mod $b = $((a%b))"
-echo "$a/$b = $((a/b))"
-echo "Thats it, thank you"
+echo "sum of $a and $b is $((a+b))"
+echo "difference of $a and $b is $((a-b))"
+echo "product of $a and $b is $((a*b))"
+echo "Quotient of $a/$b is $((a/b))"
+echo "floating division: scale=3; $a/$b | bc "
--- bashvariables.sh
+++ bashvariables.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 # storing command in variables
 PWD=$(pwd)
-list=$(ls -ail)
+list=$(ls)
+user=$(hostname)
 
-echo "present working Directory is $PWD" # $ symbol is used to invoke value of a variable
+echo "present working Directory is $PWD" 
+echo "User name $user"
+# $ symbol is used to invoke value of a variable
 echo "files in this directory are $list"
 cal # you can also directly write a command to get executed
