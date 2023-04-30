f1fd3dc547ce5f11443ca46aed728923fdc3c983
shrivatsashetty <shrivatsa307@gmail.com>
2023-04-30 19:49:51 +0530

    given execute permission to all files

--- hiscript.sh
+++ hiscript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "What is your name"
 read -r name
-echo "Hiii $name how are you"
-echo "todays date is $(date)"
+echo "Hi $name how are you"
+echo "todays date is $(date)" # '$()' is used for execution of a command
 eval ls # eval is used for execution of command
 echo "Have a nice day"	
