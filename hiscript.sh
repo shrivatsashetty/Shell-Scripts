92d1759c3233d3d65eaa0c2671f1ecd500f660ac
shrivatsashetty <shrivatsa307@gmail.com>
2023-04-30 19:32:22 +0530

    eval command

--- hiscript.sh
+++ hiscript.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "What is your name"
-read name
+read -r name
 echo "Hiii $name how are you"
-echo "todays date is `date`"
+echo "todays date is $(date)"
+eval ls #eval is used for execution of command
 echo "Have a nice day"	
