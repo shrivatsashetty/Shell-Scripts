447e675837d5b3d7a5b1512b9e95ef350d23da52
shrivatsashetty <shrivatsa307@gmail.com>
2023-04-30 21:15:24 +0530

    Arithmetic operation

--- arithmetics.sh
+++ arithmetics.sh
@@ -1,25 +1,14 @@
 #! /bin/bash
 
-# illustration of arithmetic operators
-# echo "ARITHMETIC OPERATIONS"
-# echo ""
-# echo "Enter a: "
-# read -r a
-# echo "enter b: "
-# read -r b
-# echo "$a + $b = $((a+b))"
-# echo "$a - $b = $((a-b))"
-# echo "$a * $b = $((a*b))"
-# echo "$a mod $b = $((a%b))"
-# echo "$a/$b = $((a/b))"
-# echo "Thats it, thank you"
 echo "Arithemetic operations"
 echo "Enter a value"
 read -r a
 echo "Enter b value"
 read -r b
+# mathematical expressions must be enclosed in '$(( ))'
 echo "sum of $a and $b is $((a+b))"
 echo "difference of $a and $b is $((a-b))"
 echo "product of $a and $b is $((a*b))"
 echo "Quotient of $a/$b is $((a/b))"
-echo "floating division: scale=3; $a/$b | bc "
+echo "Modulo operation: $a mod $b = $((a%b))"
+#echo "floating division: scale=3; $a/$b | bc "
